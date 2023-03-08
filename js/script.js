e5cef8c5df304d876af0c323fd7970303e5e9840
kubik <1337jomakasi@gmail.com>
2023-03-08 23:45:42 +0300

    Rectangle Tool + Fill

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
 const canvas = document.querySelector("canvas"),
+toolBtns = document.querySelectorAll(".tool"),
+fillColor = document.querySelector("#fill-color"),
 ctx = canvas.getContext("2d");
 
-let isDrawing = false,
+//Глобальные переменные с значениями по умолчанию 
+let prevMouseX, prevMouseY, snapshot,
+isDrawing = false,
+selectedTool = "brush",
 brushWidth = 5;
+
 
 window.addEventListener("load", () => {
     //Установка ширины и высоты canvas
@@ -10,17 +16,44 @@ window.addEventListener("load", () => {
     canvas.height = canvas.offsetHeight;    
 });
 
-const startDraw = () => {
+const drawRect = (e) => {
+    if(!fillColor.checked) { //Без заливки
+        return ctx.strokeRect(e.offsetX, e.offsetY, prevMouseX - e.offsetX, prevMouseY - e.offsetY);
+    }
+    // С заливкой
+    ctx.fillRect(e.offsetX, e.offsetY, prevMouseX - e.offsetX, prevMouseY - e.offsetY);    
+};
+
+const startDraw = (e) => {
     isDrawing = true; 
+    prevMouseX = e.offsetX; //Текущая позиция по Х как значение  prevMouseX
+    prevMouseY = e.offsetY; //Текущая позиция по Y как значение  prevMouseY
     ctx.beginPath(); //Создание нового пути для рисования
     ctx.lineWidth = brushWidth; //Ширина линии равна brushSize
+    snapshot = ctx.getImageData(0, 0, canvas.width, canvas.height);
 };
 
 const drawing = (e) => {
     if(!isDrawing) return; // Если isDrawing = false 
-    ctx.lineTo(e.offsetX, e.offsetY); //Cоздание линии в соответствии с указателем мыши
-    ctx.stroke();
+    ctx.putImageData(snapshot, 0, 0);
+
+    if(selectedTool === "brush") {
+        ctx.lineTo(e.offsetX, e.offsetY); //Cоздание линии в соответствии с указателем мыши
+        ctx.stroke();
+    } else if(selectedTool === "rectangle") {
+        drawRect(e);
+    }
+    
 };
+
+toolBtns.forEach(btn => {
+    btn.addEventListener("click", () => { //Добавление события по клику к каждому инструменту
+        document.querySelector(".options .active").classList.remove("active");
+        btn.classList.add("active");
+        selectedTool = btn.id;
+        console.log(btn.id);
+    });
+});
 
 canvas.addEventListener("mousedown", startDraw);
 canvas.addEventListener("mouseup", () => isDrawing = false);
